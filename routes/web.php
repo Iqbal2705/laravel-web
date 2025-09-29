0a98fc6992a22f24b8c72ee5d50eee026d8855ca
agave4 <najwan24ti@mahasiswa.pcr.ac.id>
2025-09-29 14:07:34 +0700

    ada perubahan
    test

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\MahasiswaController;
 use App\Http\Controllers\MatakuliahController;
 
+//hahahaha
 Route::get('/pcr', function () {
     return ('Selamat datang di website kampus PCR!');
 });
