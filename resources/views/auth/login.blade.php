873c06de1cd6c7d623625b622af4e4975feed5e6
iqbal <alfayyadh24ti@mahasiswa.pcr.ac.id>
2025-12-01 14:53:45 +0700

    login

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -35,7 +35,7 @@
                 <label class="block text-gray-700 text-sm font-bold mb-2">Password</label>
                 <input type="password" name="password"
                     class="w-full px-3 py-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500"
-                    placeholder="********" required>
+                    placeholder="iqbal271105" required>
             </div>
 
             <button type="submit"
